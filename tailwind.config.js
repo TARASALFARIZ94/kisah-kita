5c60ef64b30ee421676bbff345422628fe5b938a
TARASALFARIZ94 <taras.al@mhs.itenas.ac.id>
2025-06-11 15:17:58 +0700

    sec commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+     // Ini penting! Beri tahu Tailwind di mana file-file Anda berada
+    // Jika ada folder `pages` untuk Pages Router
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    // Jika ada folder `app` untuk App Router (PASTIKAN INI ADA)
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}', // Mungkin juga perlu ini jika kode Anda di dalam `src`
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
+
